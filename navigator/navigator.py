b9152b14032bfb04fe9660d1dbe8d249a3c8c12f
Rao Bierneni <bierneni.rao@gmail.com>
2018-06-07 21:11:25 -0700

    Wait for instances to stop and start during snapshit process

--- navigator/navigator.py
+++ navigator/navigator.py
@@ -70,7 +70,7 @@ def list_volumes(project):
 def instances():
     """Commands for instances"""
 
-@instances.command('snapshots',
+@instances.command('snapshot',
     help="Create snapshots of all volumes")
 @click.option('--project', default=None, help="Only instances for project (tag Project:<name>)")
 def create_snapshots(project):
@@ -79,10 +79,21 @@ def create_snapshots(project):
     instances = filter_instances(project)
 
     for i in instances:
+        print("Stopping {0}...".format(i.id))
+
         i.stop()
+        i.wait_until_stopped()
+
         for v in i.volumes.all():
-            print("Creating snapshot of {0}".format(v.id))
+            print(" Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by Navigator")
+
+        print("starting {0}...".format(i.id))
+
+        i.start()
+        i.wait_until_running()
+
+    print("Job's done!")
 
     return
 
